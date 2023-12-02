37f4891b49749dedb9b020fefb2c144415096f48
NORH123 <amnor167@gmail.com>
2023-12-02 20:41:16 +0300

    init

---
+++ src/components/memberInfo.js
@@ -0,0 +1,55 @@
+const teamMembers = [
+  {
+    id: 1,
+    img: '/images/1.png',
+    name: 'Alice Wonderland',
+    position: 'Software Engineer',
+    phone: '+1 555-1234',
+    email: 'alice@company.com',
+  },
+  {
+    id: 2,
+    img: '/images/2.png',
+    name: 'Bob Smith',
+    position: 'Product Manager',
+    phone: '+1 555-5678',
+    email: 'bob@company.com',
+  },
+  {
+    id: 3,
+    img: '/images/3.png',
+    name: 'Charlie Brown',
+    position: 'UX Designer',
+    phone: '+1 555-9876',
+    email: 'charlie@company.com',
+    website: 'charliebrown.design',
+  },
+  {
+    id: 4,
+    img: '/images/4.png',
+    name: 'David Miller',
+    position: 'Marketing Specialist',
+    phone: '+1 555-4321',
+    email: 'david@company.com',
+  },
+  {
+    id: 5,
+    img: '/images/5.png',
+    name: 'Eva Johnson',
+    position: 'CEO',
+    phone: '+1 555-8765',
+    email: 'eva@company.com',
+    website: 'evajohnson.com',
+  },
+  {
+    id: 6,
+    img: '/images/6.png',
+    name: 'Frank Davis',
+    position: 'Frontend Developer',
+    phone: '+1 555-5678',
+    email: 'frank@company.com',
+    website: 'frankdavis.dev',
+  },
+];
+
+export default teamMembers;
